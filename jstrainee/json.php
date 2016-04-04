81f9c1d99d342defc53c9a378a17ec932f37ac28
Strebkova <strebkova.o.a@gmail.com>
2016-04-04 11:41:19 +0300

    first commit

---
+++ jstrainee/json.php
@@ -0,0 +1,11 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+
+
+echo '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+
+// '<?xml version="1.0" encoding="UTF-8"><map key="a" value="1"/><map key="b" value="2"/><map key="c" value="3"/><map key="d" value="4"/><map key="e" value="5"/>'
+?>
